da36e71ebd6178dfbe00ec21d421e5b715a662d7
DmitryL <dimak.pax@gmail.com>
2023-02-07 16:39:20 +0600

    v001  lesson1 nb

---
+++ lesson1.py
@@ -0,0 +1,151 @@
+# 1 #kesha
+# name = "kesha"
+# age = 2
+# dead = 4
+# print("popugia " + name + " zabolel v " + str(age) + " umer v " + str(dead))
+
+# 2 # type 
+# int - целое   stroka >> chislo => int(stroka)
+# float - дроб лайк 2.3
+# string - stroka   chislo >> stoku => str(chislo)
+# bool - True or False
+# noone - nichegoooo
+
+# 3 # vivod
+#     #print   =sep razdel =end
+# name = "kesha"
+# age = 2
+# print(name, age, sep='/')
+# #-------------------------
+# print(name)
+# print(age)
+# print("=>>>>>")
+# print(name, end=";")
+# print(age, end=";")
+
+# 4 # vvod
+#  input =>>> f.x. #
+# name2 = input() 
+# print(name2)
+# name = input('vvedite chislo: ')
+# print("privet", name)
+
+# age = int(input('Skoliho vam let: '))
+# periud = 20
+# age_periud = age + periud
+# print(age_periud)
+
+# 5 arefm operac
+#// c okr do cel chisla
+# % opred ostatok
+
+# 5 cikl
+
+
+# god = int(input("Vvedite kakoi sechas god: "))
+# while god != 2023:
+#     print("ne verno")
+#     god = int(input("Vvedite kakoi sechas god: "))
+# print("ti znal :D ")
+
+# vivod chisel ot 0 do 100
+# chislo = 0
+# while chislo < 101:
+#     print(chislo)
+#     chislo += 1
+# print("gotovo")
+
+#vivod chisel ot 0 do n(zadaet polzovatel)
+# chislo = 0
+# numFromUser = int(input("vvedite chislo: "))
+# while chislo <= numFromUser:
+#     print(chislo)
+#     chislo += 1
+# print("gotovo")
+
+#vivod chet chisel ot 0 do n
+# chislo = 0
+# numFromUser = int(input("vvedite chislo: "))
+# while chislo <= numFromUser:
+#     if chislo % 2 == 0:
+#         print(chislo)
+#     chislo += 1    
+    
+# print("gotovo")
+
+# 6 konstruct break (vihodit iz cikla ne vajno vipolneni li uslovia ili net)
+# god = None
+# while god != 2023:
+#     god = int(input("Vvedite kakoi sechas god: "))
+#     if god == 2023:
+#         break        #vihod iz cikla
+#     print("ne verno")
+# print("ti znal :D ")
+
+# ===>
+
+# god = None
+# while True:
+#     god = int(input("Vvedite kakoi sechas god: "))
+#     if god == 2023:
+#         break        #vihod iz cikla
+#     print("ne verno")
+# print("ti znal :D ")
+
+#7 comtinue (perehod na sled iteracii cikla. komandi v
+# cikle posle continue ne vipolniautsia)
+#vivod chet chisel ot 0 do n
+# chislo = 0
+# numFromUser = int(input("vvedite chislo: "))
+# while chislo <= numFromUser:
+#     if chislo % 2 == 0:
+#         print(chislo)
+#     chislo += 1   
+# print("gotovo")
+
+# ===>
+
+# chislo = 0
+# numFromUser = int(input("vvedite chislo: "))
+# while chislo <= numFromUser:
+#     if chislo % 2 == 0:
+#         chislo += 1 
+#         continue
+#     print(chislo)
+#     chislo += 1
+# print("gotovo - vivod vsex nechetnih chisel v promejutke ot 0"
+#   + " do " + str(numFromUser))
+
+#8 while-else(v bloke else(posle while) mi vipolniaem deistvie posle togo kak
+# vishli iz cikla while kogda uslovie cikla stalo nevernim(false))
+
+# number = 0
+# while number <= 20:
+#     print(number)
+#     number += 1
+#     # if number == 15:
+#     #     break
+# else: print("else - vse")
+# print("vse")
+
+        
+        
+      
+
+    
+
+
+
+    
+    
+
+
+
+
+
+    
+
+
+
+
+
