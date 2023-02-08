25c486a37578f03e132e40432d1fa9d39eb08416
DmitryL <dimak.pax@gmail.com>
2023-02-08 18:45:14 +0600

    v002 Done

--- lesson3HW.py
+++ lesson3HW.py
@@ -2,6 +2,10 @@
 #  прибавьте к числу 2 и выведите результат на экран. 
 # Если возникла ошибка, прочитайте ее, вспомните урок и 
 # постарайтесь устранить ошибку.
+
+# valueFromUser = int(input("Enter the number: "))
+# print(valueFromUser + 2)
+
 # 2: Используя цикл, запрашивайте у пользователя число, 
 # пока оно не станет больше 0, но меньше 10.
 # После того, как пользователь введет корректное число,
@@ -11,6 +15,15 @@
 #  И просите ввести заново.
 # Допустим, пользователь ввел 2, оно подходит.
 #  Возводим его в степень 2 и выводим 4.
+
+# numberFromUser = None
+# while True:
+#     numberFromUser = int(input("Enter the numbers. 0 < numbers < 10:  "))
+#     if numberFromUser > 0 and numberFromUser < 10:
+#         break
+#     print("Oshibka.")
+# print("Vvedenoe vami chislo vo 2 stepeni ravno: " + str(numberFromUser ** 2))
+
 # 3: Создайте программу “Медицинская анкета”,
 #  где вы запросите у пользователя следующие данные:
 #  имя, фамилия, возраст и вес.
@@ -23,3 +36,23 @@
 #  более 40 и вес менее 50 или больше 120 кг.
 # Все остальные варианты вы можете обработать
 #  на ваш вкус и полет фантазии.
+def ScanUserMetric(weight, age, user):
+    if age < 30 and 50 <= weight <= 120:
+        return user + " - Вы богатырь, может 50 килограммовый, но богатырь"
+    elif 30 <= age < 40  and (weight < 50 or weight > 120):
+        return user + " - Вы исчезаете или же вы фанат грузового лифта. Займитесь собой"
+    elif 40 <= age < 100  and (weight < 50 or weight > 120):
+        return user + " - Срочно обратитесь к лекарю" 
+    elif age >= 100:
+        return user + " - Jesus Christ, That's Jason Bourne"
+firstName = input("Введите свое имя: ")
+secondName = input("Введите свою фамилию: ")
+user = firstName + " " + secondName
+weight = int(input("Введите свой вес: "))
+age = int(input("Введите свой возраст: "))
+print(ScanUserMetric(weight, age, user))
+
+
+
+
+
